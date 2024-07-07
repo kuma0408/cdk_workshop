fa8ead461b0309df4e6296e8b96dc7252f1f17d0
kuma0408 <gigglingirl1225@gmail.com>
2024-07-07 11:51:20 +0900

    途中までコミット

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,19 +1,26 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import *  as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
-export class CdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // defines an AWS lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from the "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    })
+
+    const hellotWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
     });
 
-    const topic = new sns.Topic(this, 'CdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hellotWithCounter.handler
+    })
   }
 }
